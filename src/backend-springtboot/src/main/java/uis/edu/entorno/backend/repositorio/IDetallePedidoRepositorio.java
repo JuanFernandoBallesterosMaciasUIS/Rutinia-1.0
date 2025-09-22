109f19dd11eb5dee0234e2755e52c031f47c19fa
Alvoid101 <10101010alvoid@gmail.com>
2025-09-22 15:24:56 -0500

    chore: remover archivos innesesarios

--- src/backend-springtboot/src/main/java/uis/edu/entorno/backend/repositorio/IDetallePedidoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package uis.edu.entorno.backend.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import uis.edu.entorno.backend.modelo.DetallePedido;
-
-public interface IDetallePedidoRepositorio extends JpaRepository<DetallePedido, Integer>{
-
-}
