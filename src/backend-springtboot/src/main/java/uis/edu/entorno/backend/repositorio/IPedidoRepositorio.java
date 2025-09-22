409a12fafb4a8de8b75f58d695d14acf9a7600e6
Alvoid101 <10101010alvoid@gmail.com>
2025-09-22 15:32:11 -0500

    chore: remover archivos inesesarios

--- src/backend-springtboot/src/main/java/uis/edu/entorno/backend/repositorio/IPedidoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package uis.edu.entorno.backend.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import uis.edu.entorno.backend.modelo.Pedido;
-
-public interface IPedidoRepositorio extends JpaRepository<Pedido, Integer> {
-
-}
