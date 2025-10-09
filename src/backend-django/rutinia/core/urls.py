a7fdd87a7be293fceda6866f4f10d3fff2cc4157
Alvoid101 <10101010alvoid@gmail.com>
2025-10-08 20:28:32 -0500

    fix(registro): cambiar url registros
    antes tenia la url api/habitos/registros esto causaba que se accediera al endpoint de habitos en lugar de el de registros

--- src/backend-django/rutinia/core/urls.py
+++ src/backend-django/rutinia/core/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import UsuarioViewSet, ToolViewSet, RolViewSet, CategoriaViewSet, RegistroHabitoViewSet
+from .views import UsuarioViewSet, ToolViewSet, RolViewSet, HabitoViewSet, CategoriaViewSet, RegistroHabitoViewSet
 from django.urls import path, include
 
 router = DefaultRouter()
@@ -7,7 +7,8 @@ router.register(r'usuarios', UsuarioViewSet, basename='usuario')
 router.register(r'roles', RolViewSet, basename="rol")
 router.register(r'tools', ToolViewSet, basename='tool')
 router.register(r'categorias', CategoriaViewSet, basename='categoria')
-router.register(r'habitos/registros', RegistroHabitoViewSet, basename='registro')
+router.register(r'habitos', HabitoViewSet, basename="habito")
+router.register(r'registros', RegistroHabitoViewSet, basename='registro')
 urlpatterns = [
     path('', include(router.urls)),
 ]
