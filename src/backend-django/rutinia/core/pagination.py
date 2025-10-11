90989e6353127912cf48233a1a543387acbcf88c
Alvoid101 <10101010alvoid@gmail.com>
2025-10-11 15:23:49 -0500

    fix(habitos): agrandar tamaño de paginacion

--- src/backend-django/rutinia/core/pagination.py
+++ src/backend-django/rutinia/core/pagination.py
@@ -1,7 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 
 class HabitoPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    page_query_param = 'page'
+    page_size = 20 #Este atributo me indica el numero de habitos que voy a obtener por pagina
+    page_size_query_param = 'page_size' #Con este atributo puedo cambiar la cantidad de habitos que veo en lugar de los por defecto 
+    page_query_param = 'page' #en la url puedo pasar a otra pagina por ejemplo ?page_size=2
     max_page_size = 50
