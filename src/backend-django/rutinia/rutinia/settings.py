d987366bbee321661afc2ffd52bec078ffd75de6
Juan Fernando Ballesteros Macias <jballesterosmacias23@gmail.com>
2025-10-14 13:54:08 -0500

    feat: implementar mejoras de seguridad, mover SECRET_KEY a variables de entorno y actualizar .gitignore

--- src/backend-django/rutinia/rutinia/settings.py
+++ src/backend-django/rutinia/rutinia/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g%w-@c7bh&bp51@=b)kr*ilsjna*q&(snxkptz)ba@i2*_i6p-'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-CHANGE-THIS-IN-PRODUCTION')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
